171c3a16065d2528a24b80b5353a27414324a5f6
Филипп Громов <dropkick_grip@mail.ru>
2022-11-18 20:36:17 +0500

    Версия операционной системы 14 практическая работа выполнена

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,50 @@
-# TODO здесь писать код
+def separation_whole(number):
+    flag = True
+    whole = ""
+    fraction = ""
+    for symbol in number:
+        if symbol == '.':
+            flag = False
+        elif flag:
+            whole = whole + symbol
+        else:
+            fraction = fraction + symbol
+    return int(whole)
+
+def separation_fraction(number):
+    flag = True
+    whole = ""
+    fraction = ""
+    for symbol in number:
+        if symbol == '.':
+            flag = False
+        elif flag:
+            whole = whole + symbol
+        else:
+            fraction = fraction + symbol
+    return int(fraction)
+
+def revers(n):
+    count = 0
+    while n > 0:
+        count = count * 10 + n % 10
+        n //= 10
+    return str(count)
+
+def bonding(revers_whole, revers_fraction):
+    revers_num = revers_whole + "." + revers_fraction
+    return str(revers_num)
+
+def summ_mun(first_num, second_num):
+    summ = float(first_num) + float(second_num)
+    return summ
+
+first_number = (input("Введите первое число: "))
+second_number = (input("Введите второе число: "))
+
+print(f"\nПервое число наоборот: {bonding(revers(separation_whole(first_number)), revers(separation_fraction(first_number)))}"
+f"\nВторое число наоборот: {bonding(revers(separation_whole(second_number)), revers(separation_fraction(second_number)))}")
+
+print(f"\nСумма: {summ_mun(bonding(revers(separation_whole(first_number)), revers(separation_fraction(first_number))), bonding(revers(separation_whole(second_number)), revers(separation_fraction(second_number))))}")
 
 
--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+import math
+
+def check_coordinate(x, y, r):
+    return (x * x + y * y) ** 0.5 < r
+
+
+print('Введите координаты монетки:')
+coordinate_x = float(input('X: '))
+coordinate_y = float(input('Y: '))
+area = int(input('Введите радиус: '))
+
+if check_coordinate(coordinate_x, coordinate_y, area):
+    print('Монетка где-то рядом.')
+else:
+    print('Монетки в области нет.')
