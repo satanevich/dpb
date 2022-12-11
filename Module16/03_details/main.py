85271c9a39c6dbe0f80a7b5c8982b69c38443ac5
Филипп Громов <dropkick_grip@mail.ru>
2022-12-11 20:25:44 +0500

    Выполнено

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,12 +2,16 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
+#name = input('Название детали: ')
+#count = 0
+#price_sum = 0
+#for product in shop:
+#    if product[0] == name:
+#        count += product.count(name)
+#        price = product[1]
+#        price_sum += price
+#print(f'\nНазвание детали: {name}\nКол-во деталей - {count}\nОбщая стоимость - {price_sum}')
+
 name = input('Название детали: ')
-count = 0
-price_sum = 0
-for product in shop:
-    if product[0] == name:
-        count += product.count(name)
-        price = product[1]
-        price_sum += price
-print(f'\nНазвание детали: {name}\nКол-во деталей - {count}\nОбщая стоимость - {price_sum}')
+prices = [product[1] for product in shop if product[0] == name]
+print(f'Кол-во деталей - {len(prices)}\nОбщая стоимость - {sum(prices)}')
--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -4,19 +4,19 @@ list_two = []
 for i in range(3):
         query = 'Введите ' + str(i + 1) + ' число для первого списка: '
         number = int(input(query))
-        lst_one.append(number)
+        list_one.append(number)
 
 for i in range(7):
         query = 'Введите ' + str(i + 1) + ' число для второго списка: '
         number = int(input(query))
-        lst_two.append(number)
+        list_two.append(number)
 
 print('Первый список:', list_one)
 print('Второй список:', list_two)
 
-list_one.extend(lst_two)
-for _ in range(len(lst_one)):
-        for i in lst_one:
-                if lst_one.count(i) > 1:
-                        lst_one.remove(i)
-print('Новый первый список с уникальными элементами:', lst_one)
+list_one.extend(list_two)
+for _ in range(len(list_one)):
+        for i in list_one:
+                if list_one.count(i) > 1:
+                        list_one.remove(i)
+print('Новый первый список с уникальными элементами:', list_one)
