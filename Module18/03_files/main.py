4cf4afc20c8b945e87f912bbf929d3a09177edcf
Филипп Громов <dropkick_grip@mail.ru>
2022-12-18 22:33:43 +0500

    Промежуточная проверка

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+# spec_sym = list('@№$%^&*()')
+#extensions = ('.txt', '.docx')
+file_name = input('Название файла: ').lower()
+
+# if file_name[0] in spec_sym:
+#     print('Ошибка: название начинается на один из специальных символов')
+if file_name.startswith(("@", "№", "$", "%", "^", "&", "*", "(", ")")):
+    print('Ошибка: название начинается на один из специальных символов')
+elif not file_name.endswith(('.txt', 'docx')):
+    print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+else:
+    print('Файл назван верно.')
