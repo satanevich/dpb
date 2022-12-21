e962eea05b67448a956eaaec1a0524db236516d6
Филипп Громов <dropkick_grip@mail.ru>
2022-12-21 20:41:22 +0500

    Complite

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+text = input('Введите текст: ')
+sym_dict = dict()
+text_dict = dict()
+
+print('Оригинальный словарь частот:')
+for sym in text:
+    if sym in sym_dict:
+        sym_dict[sym] += 1
+    else:
+        sym_dict[sym] = 1
+for i_sym in sorted(sym_dict.keys()):
+    print(i_sym, ': ', sym_dict[i_sym], sep='')
+
+print('Инвертированный словарь частот:')
+for i_letter, i_num in sym_dict.items():
+    text_dict.setdefault(i_num, []).append(i_letter)
+for i in text_dict:
+    print(i, ': ', text_dict[i], sep='')
