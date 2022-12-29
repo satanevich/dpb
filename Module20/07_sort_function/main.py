30614eeb745d3330b0e6e780d608fad39439bfb7
satanevich <diezharakiri@gmail.com>
2022-12-29 23:18:14 +0500

    Complite

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,8 +1,8 @@
-
-def tpl_sort(tpl):
-    for element in tpl:
-        if not isinstance(element, int):
-            return tpl
-    return tuple(sorted(tpl))
+def tpl_sort(a, b, c, d, f, g, k):
+    mylist = a, b, c, d, f, g, k
+    if all(type(item) is int for item in mylist):
+        return sorted(mylist)
+    else:
+        return mylist
 
 print(tpl_sort(6, 3, -1, 8, 4, 10, -5))
