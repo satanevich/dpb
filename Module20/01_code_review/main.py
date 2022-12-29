4ab0873ee5772716ee3d8ff6b65ccc1f417cc539
satanevich <diezharakiri@gmail.com>
2022-12-29 22:48:14 +0500

    Uncomplite

--- Module20/01_code_review/main.py
+++ Module20/01_code_review/main.py
@@ -1,3 +1,6 @@
+def foo(d: dict) -> tuple:
+    return set(sum([d[i]['interests'] for i in d], [])), sum(map(len, [d[i]['surname'] for i in d]))
+
 students = {
     1: {
         'name': 'Bob',
@@ -19,26 +22,29 @@ students = {
     }
 }
 
+interests, total_len = foo(students)
+print('Полный список интересов всех студентов:', ', '.join(interests))
+print('Общая длина всех фамилий студентов:', total_len)
 
-def f(dict):
-    lst = []
-    string = ''
-    for i in dict:
-        lst += (dict[i]['interests'])
-        string += dict[i]['surname']
-    cnt = 0
-    for s in string:
-        cnt += 1
-    return lst, cnt
+# def f(dict):
+#     lst = []
+#     string = ''
+#     for i in dict:
+#         lst += (dict[i]['interests'])
+#         string += dict[i]['surname']
+#     cnt = 0
+#     for s in string:
+#         cnt += 1
+#     return lst, cnt
+#
+#
+# pairs = []
+# for i in students:
+#     pairs += (i, students[i]['age'])
+#
+#
+# my_lst = f(students)[0]
+# l = f(students)[1]
+# print(my_lst, l)
 
 
-pairs = []
-for i in students:
-    pairs += (i, students[i]['age'])
-
-
-my_lst = f(students)[0]
-l = f(students)[1]
-print(my_lst, l)
-
-# TODO исправить код
